f32fb4d7ebdb00405f7e48b6f75763851e2f445a
Michelle Lytle <mnlytle@ucsc.edu>
2021-04-07 14:31:44 -0700

    background

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,7 +3,16 @@ class Play extends Phaser.Scene {
         super("playScene");
     }
 
+    preload() {
+        this.load.image('starfield', '/assets/starfield.png');
+    }
+
     create() {
+
+        this.starfield = this.add.tileSprite(
+            0, 0, 640, 480, 'starfield'
+        ).setOrigin(0, 0);
+
         // green UI background 
         this.add.rectangle(
             0, 
@@ -19,5 +28,10 @@ class Play extends Phaser.Scene {
 	    this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 
+
+    }
+
+    update() {
+        this.starfield.tilePositionX -= 4;
     }
 }
